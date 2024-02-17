7badda88a0e2ac7e7bbe659af4e1c3ec56778d84
Md. Shoeb Adnan <madnan172095@bscse.uiu.ac.bd>
2024-02-17 20:44:48 +0600

    First Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var gg: Float = (9/10)
+print(gg)
---
+++ calculator_autolayout/calculator_autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator_autolayout
+//
+//  Created by Shoeb Adnan on 5/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
